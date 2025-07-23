f0663deb55e8dcf8fa45af2e9bbdc7bc552ff40e
Angela1410 <angelagramatica1410@gmail.com>
2025-07-23 10:09:45 +0800

    Add files via upload

---
+++ activity2_number_printer.py
@@ -0,0 +1,6 @@
+# number_printer.py
+for num in range(1, 11):
+    if num % 2 == 0:
+        print(f"{num} - Even")
+    else:
+        print(f"{num} - Odd")
---
+++ activity3_login_system.py
@@ -0,0 +1,13 @@
+# login_system.py
+username = "admin"
+password = "python123"
+
+while True:
+    input_user = input("Enter username: ")
+    input_pass = input("Enter password: ")
+    
+    if input_user == username and input_pass == password:
+        print("Login successful!")
+        break
+    else:
+        print("Invalid credentials. Try again.")
